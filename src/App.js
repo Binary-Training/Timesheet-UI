fb4cbfe0fc97ec3936e3ce57c8b0f21438cbb9e3
vidhubinary <102211626+vidhubinary@users.noreply.github.com>
2022-04-01 13:01:06 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Picker from "./components/Picker";
+import MainTable from "./components/Table";
+
+function App() {
+  return (
+    <>
+    {/* <Picker /> */}
+
+    <MainTable />
+    </>
+  );
+}
+
+export default App;
